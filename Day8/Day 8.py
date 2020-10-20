04a24f38f483143190817cdc2ea9ac8d8e69dceb
sampada101 <55329464+sampada101@users.noreply.github.com>
2020-10-20 21:55:40 +0545

    Create Day 8.py

---
+++ Day8/Day 8.py
@@ -0,0 +1,15 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = int(radius)
+
+    def getCircumference(self):
+        return 2*22/7*self.radius
+
+    def getArea(self):
+        return 22/7*self.radius*self.radius
+
+
+radius = input('Radius of the circle: ')
+circle = Circle(radius)
+print(f'The Circumference of the circle is: {circle.getCircumference()}')
+print(f'The area of the cirlce is: {circle.getArea()}')
